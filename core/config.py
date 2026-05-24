53da2dbf18147070b9990791bdda96bd8d9ee779
Marc-Antoine <2360530@cegepoutaouais.qc.ca>
2026-05-23 21:26:59 -0400

    Set up project structure with database configuration, Pydantic models, and dependencies.

---
+++ core/config.py
@@ -0,0 +1,19 @@
+from functools import lru_cache
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    app_name: str = "Ticket Assistant API"
+    database_url: str
+    jwt_secret: str
+
+    db_user: str = "DB_USER"
+    db_password: str = "DB_PASSWORD"
+    db_host: str = "DB_HOST", "localhost"
+    db_port: str = "DB_PORT", "5432"
+    db_name: str = "DB_NAME"
+
+
+@lru_cache
+def get_settings() -> Settings:
+    return Settings()
---
+++ db/session.py
@@ -0,0 +1,25 @@
+from contextlib import contextmanager
+from core.config import get_settings
+
+import psycopg
+
+
+class DbSession:
+    """Classe de configuration pour gérer les sessions PostgreSQL."""
+
+    def __init__(self, database_url):
+        self.db_url = f"postgresql://{get_settings().db_user}:{get_settings().db_password}@{get_settings().db_host}:{get_settings().db_port}/{get_settings().dbname}"
+
+    @contextmanager
+    def get_cursor(self):
+        """Gestionnaire de contexte pour obtenir un curseur SQL sécurisé."""
+
+        with psycopg.connect(self.db_url) as conn:
+            with conn.cursor() as cur:
+                try:
+                    yield cur
+                except Exception:
+                    raise
+
+
+db = DbSession(get_settings().database_url)
